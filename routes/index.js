1d8a46b53b6696825f862178553ea8fd91387865
Rishabh Shukla <127231574+rishabhshuklayt@users.noreply.github.com>
2024-07-25 22:48:05 +0530

    Update index.js
    just updated the route protection of is looged in on the main route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const isLoggedIn = require('../middleware/isLoggedIn')
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', isLoggedIn ,function(req, res, next) {
+  res.render('index', { student: req.student });
 });
 
 module.exports = router;
