2b54540e35c77eee524d2dea8896a91155784aa6
Rishabh Shukla <127231574+rishabhshuklayt@users.noreply.github.com>
2024-07-26 13:51:38 +0530

    Update app.js
    updated the app.js for sending data to client

--- app.js
+++ app.js
@@ -1,87 +1,114 @@
-var createError = require('http-errors');
-const port = 3000
-var express = require('express');
-var session = require('express-session');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var authRouter = require('./routes/auth')
-var mongooseConnection = require('./config/mongoose')
-require('dotenv').config()
+var createError = require("http-errors");
+const port = 3000;
+var express = require("express");
+var session = require("express-session");
+const studentModel = require("./model/student");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var authRouter = require("./routes/auth");
+var mongooseConnection = require("./config/mongoose");
+var jwt = require('jsonwebtoken')
+const isLoggedIn = require('./middleware/isLoggedIn'); // Add the middleware
+require("dotenv").config();
 
 var app = express();
+app.use(cookieParser());
 
-//setting up socket.io
-const socketio = require('socket.io')
-const http = require('http');
-// const { connection } = require('mongoose');
-const server = http.createServer(app)
-const io = socketio(server)
+// Setting up socket.io
+const socketio = require("socket.io");
+const http = require("http");
+const server = http.createServer(app);
+const io = socketio(server);
 
-io.on("connection",function(socket){
+// Use the middleware to get the user data
+io.use(async (socket, next) => {
+    const token = socket.handshake.headers.cookie?.split('token=')[1];
+    if (!token) {
+        return next(new Error('Authentication error'));
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SSH);
+        const student = await studentModel.findById(decoded._id);
+        if (!student) {
+            return next(new Error('Authentication error'));
+        }
+        socket.user = student;
+        next();
+    } catch (error) {
+        console.error('Error during token verification:', error);
+        return next(new Error('Authentication error'));
+    }
+});
 
-  socket.on('send-location', (data) => {
-    console.log(data)
-    io.emit('receive-location', { id: socket.id, ...data });
-    }) 
-    console.log(" socket io connected")
+io.on("connection", function (socket) {
+    socket.on("send-location", async (data) => {
+        try {
+            const userData = socket.user;
+            if (userData) {
+                io.emit("receive-location", {
+                    id: socket.id,
+                    ...data,
+                    user: {
+                        name: `${userData.firstName} ${userData.lastName}`,
+                        avatar: userData.avatar.toString('base64'),
+                    }
+                });
+            }
+        } catch (err) {
+            console.error("Error fetching user data:", err);
+        }
+    });
 
+    console.log("Socket.io connected");
 
-    socket.on('disconnect',(data)=>{
-      io.emit('user-disconnected', socket.id)
-    })
-})
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+    socket.on("disconnect", () => {
+        io.emit("user-disconnected", socket.id);
+    });
+});
 
-app.use(logger('dev'));
+// View engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Express session middleware
-
 app.use(session({
-  secret: process.env.JWT_SSH, // change this to a random string
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false } // if using https, set to true
+    secret: process.env.JWT_SSH,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
 }));
 
-// ---------end of session-----------------------
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/auth", authRouter);
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/auth', authRouter);
-
-//test routes
-
-app.get('/test',(req,res)=>{
-  res.render('testmap')
-})
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Test routes
+app.get("/test", (req, res) => {
+    res.render("testmap");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
-server.listen(port,()=>{
-  console.log(`server isrunning on port ${port}`)
-})
+// Error handler
+app.use(function (err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    res.status(err.status || 500);
+    res.render("error");
+});
 
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
